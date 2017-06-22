193657d73c300b4e2585b826f5a4243ea157a60d
Peaceful Stranger <hello@world.zzz>
2017-06-22 02:43:42 +0000

    Cleaned-up start

--- do_setup_script.sh
+++ do_setup_script.sh
@@ -15,3 +15,5 @@ for i in {1..$SERVERS}
 do
     D_NAME="tStore $i"
     D_REGION=${REGION[i-1]}
+    D_IMAGE=$DROPLET_ID
+    curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d '{"name":"tStore.'$TAG'.'$i'","region":"'${REGION[i-1]}'","size":"512mb","image":"'$DROPLET_ID'","ssh_keys":"10168822","monitoring":"True","tags":["tStore","'$TAG'"]}' "https://api.digitalocean.com/v2/droplets"
