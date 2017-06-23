bae76caa0d289ec7e18ccf7dabd6865025370c47
Peaceful Stranger <howboutno@why.net>
2017-06-23 16:45:12 -0400

    maybe this? pt3 the arghening

--- no_docker_setup.sh
+++ no_docker_setup.sh
@@ -23,7 +23,7 @@ done
 
 for (( i=0; i<=$SERVERS; i++ ))
 do
-    until [ $(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" "https://api.digitalocean.com/v2/droplets?tag_name=$NAME" | jq '.droplets['$i'].networks.v4[0].ip_address') != "null" ];
+    until [ -a $(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" "https://api.digitalocean.com/v2/droplets?tag_name=$NAME" | jq '.droplets['$i'].networks.v4[0].ip_address') ];
     do
           sleep 1m
     done
