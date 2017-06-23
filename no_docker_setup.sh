a448e7f2342223c92fce6e7a92e6623e832b8bc7
Peaceful Stranger <howboutno@why.net>
2017-06-23 16:23:52 -0400

    Testing delay on script for getting IP addresses

--- no_docker_setup.sh
+++ no_docker_setup.sh
@@ -23,8 +23,11 @@ done
 
 for (( i=0; i<=$SERVERS; i++ ))
 do
-    ADDRESSES[$i]=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" "https://api.digitalocean.com/v2/droplets?tag_name=$NAME" | jq '.droplets['$i'].networks.v4[0].ip_address')
-    echo ${ADDRESSES[$i]}
+    until [ -a $(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" "https://api.digitalocean.com/v2/droplets?tag_name=$NAME" | jq '.droplets['$i'].networks.v4[0].ip_address')]
+          sleep 1m
+    done
+        ADDRESSES[$i]=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" "https://api.digitalocean.com/v2/droplets?tag_name=$NAME" | jq '.droplets['$i'].networks.v4[0].ip_address')
+
 done
 
 for (( i=0; i<=$SERVERS; i++ ))
