f423731b5c28f585b7b112b51245d1ea9a99cfca
Peaceful Stranger <howboutno@why.net>
2017-06-24 14:22:14 -0400

    maybe this time?

--- no_docker_setup.sh
+++ no_docker_setup.sh
@@ -16,14 +16,14 @@ NAME=test
 
 REGIONS=(nyc1 nyc2 nyc3 sfo1 sfo2 tor1 fra1 ams2 ams3 blr1)
 
-for (( i=0; i<=$SERVERS; i++ ))
+for (( i=1; i<=$SERVERS; i++ ))
 do
-    curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d '{"name":"tStore.'$NAME'.'$i'","region":"'${REGIONS[$i]}'","size":"512mb","image":"'$DROPLET_ID'","ssh_keys":["10168822"],"monitoring":"True","tags":["tStore","'$NAME'"]}' "https://api.digitalocean.com/v2/droplets"
+    curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d '{"name":"tStore.'$NAME'.'$i'","region":"'${REGIONS[$i-1]}'","size":"512mb","image":"'$DROPLET_ID'","ssh_keys":["10168822"],"monitoring":"True","tags":["tStore","'$NAME'"]}' "https://api.digitalocean.com/v2/droplets"
 done
 
 for (( i=0; i<=$SERVERS; i++ ))
 do
-    until [ -z $(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" "https://api.digitalocean.com/v2/droplets?tag_name=$NAME" | jq '.droplets['$i'].networks.v4[0].ip_address') ];
+    until [ -z "$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" "https://api.digitalocean.com/v2/droplets?tag_name=$NAME" | jq '.droplets['$i'].networks.v4[0].ip_address')" ];
     do
         echo "Wake me up later"
         sleep 1m
