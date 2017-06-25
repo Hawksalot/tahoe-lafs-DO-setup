3120f78211aea81757e495f54d91d84841d14fd1
Peaceful Stranger <howboutno@why.net>
2017-06-25 19:55:49 -0400

    first bit of work on python script

--- attempt4.py
+++ attempt4.py
@@ -1,10 +1,25 @@
 from requests import get, post
 from os import environ
+from digitalocean import Droplet, Manager
 
 # temp hardcoded variables
 servers = 10
 name = test
 regions = ["nyc1", "nyc2", "nyc3", "sfo1", "sfo2", "tor1", "fra1", "ams2", "ams3", "blr1"]
 
-for i in regions:
+token = os.environ['TOKEN']
+image = os.environ['DROPLET_ID']
+
+manager = digitalocean.Manager(token=token)
+keys = manager.get_all_sshkeys()
+
+for index, region in enumerate(regions):
+    newDroplet = digitalocean.Droplet(token=token,
+                                      name='tStore' + '-' + name + '-' + i,
+                                      region=region,
+                                      image=image,
+                                      size_slug='512mb',
+                                      ssh_keys=keys,
+                                      monitoring=True)
+    newDroplet.create()
     
